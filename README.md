f0c2bec2ed61d85185b827d7af4d428d03833546
azrakgn <akgunazra2004@gmail.com>
2026-05-01 13:37:26 +0300

    The project skeleton,package structure and .gitignore have been added

---
+++ README.md
@@ -0,0 +1,1 @@
+Objective II - SE116 Project
---
+++ src/game/main/ObjectVilleGame.java
@@ -0,0 +1,8 @@
+package game.main;
+
+public class ObjectVilleGame {
+    public static void main (String [] args){
+        System.out.println("ObjectVİLLE II has started...");
+
+    }
+}
