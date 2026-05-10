bc7490ab905d86dd5754983d6d2ffe40072b377d
azrakgn <akgunazra2004@gmail.com>
2026-05-10 18:41:40 +0300

    Implemented Manhattan distance

--- src/game/model/Cell.java
+++ src/game/model/Cell.java
@@ -22,8 +22,18 @@ public abstract class Cell {
     public char getSymbol() {
         return symbol;
     }
-    public abstract boolean isConnectable();  //Elektrik ve su geçişi için
-    public abstract boolean isZone();         //bölge kontrolü konut,ticari,endüstriyel
-    public abstract boolean isEmpty();        //hücrenin boş olup olmadığı kontrolü
+    public abstract boolean isConnectable();  // Checks if the cell allows electricity and water to pass through(roads)
+    public abstract boolean isZone();         // Checks if the cell is a specific zone (Residential, Commercial, or Industrial)
+    public abstract boolean isEmpty();        // Checks whether the cell is completely empty
+
+
+    public int calculateManhattanDistance(Cell otherCell){      //Calculates the Manhattan distance between this cell and other cell.
+        int rowDifference = Math.abs(this.row- otherCell.row);  //The formula: d = |x1 - x2| + |y1 - y2|
+        int colDifference = Math.abs(this.col - otherCell.col);
+        return rowDifference + colDifference;                   // Total steps required to reach the destination
+    }
+    public boolean isWithinServiceRadius  (Cell providerCell,int radius){
+        return this.calculateManhattanDistance(providerCell) <= radius;
+    }
 }
 
