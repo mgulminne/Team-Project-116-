c52b800482fd7a314cf934adb3f84cd8a1106cd8
deryaalpagot <deryaalpagot@gmail.com>
2026-05-17 17:09:30 +0300

    Added Housing level decrease rules

--- src/game/model/Housing.java
+++ src/game/model/Housing.java
@@ -6,6 +6,7 @@ public class Housing extends Zone {
     @Override
     public void updateZone() {
 
+        // calculate min utility
         int m = receivedElectricity;
 
         if (receivedWater < m) {
@@ -16,36 +17,45 @@ public class Housing extends Zone {
             m = receivedInternet;
         }
 
+        // level update rules
         if ( m == 0) {
             level = 0;
-            output = 0;
 
         }
         else {
-             if (level == 0 &&
-                     receivedElectricity > 0 &&
-                     receivedWater > 0 &&
-                     receivedInternet > 0)  {
+             if (level == 0 && receivedElectricity > 0 && receivedWater > 0 && receivedInternet > 0)  {
 
                  level = 1;
              }
 
-             else if (level == 1 &&
-                     hasSecurity &&
-                     hasHealth &&
-                     hasEducation ) {
+             else if (level == 1 && hasSecurity && hasHealth && hasEducation ) {
 
                  level = 2;
              }
 
-             else  if (level == 2 &&
-             receivedLifestyle > 0) {
+             else  if (level == 2 && receivedLifestyle > 0) {
 
                  level = 3;
 
+             } else if (level == 3 &&
+                     receivedLifestyle == 0) {
+
+                 level = 2;
+
+             } else if (level == 2 &&
+                     (!hasSecurity || !hasHealth || !hasEducation)) {
+
+                 level = 1;
+
              }
 
-             if (level == 1 ) {
+
+             // output calc
+             if (level == 0) {
+                 output = 0;
+             }
+
+            else if (level == 1 ) {
                  output = m;
 
              }
