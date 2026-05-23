154104d15f06a6f07efb7a211f121050beb511ce
cerensazan <ceren.sazan12@gmail.com>
2026-05-23 15:19:49 +0300

    update UtilityBfs.java

--- src/game/model/UtilityBfs.java
+++ src/game/model/UtilityBfs.java
@@ -6,8 +6,8 @@ import java.util.Queue;
 public class UtilityBfs {
 
     // up,down,left,right
-    private static final int[] rowDirections= {-1,1,0,0};
-    private static final int[] colDirections={0,0,-1,1};
+    private static final int[] rowDirections = {-1,1,0,0};
+    private static final int[] colDirections = {0,0,-1,1};
 
     public void runBfs(City city,Cell startCell){
         boolean [][] visited = new boolean[city.getRowCount()][city.getColCount()];  //keeps visited cells
@@ -17,7 +17,26 @@ public class UtilityBfs {
 
         visited[startCell.getRow()][startCell.getCol()]=true;
 
+        while (!queue.isEmpty()){
+            Cell current = queue.poll();
+
+            //check 4 directions
+            for (int i=0;i<4;i++){
+                int nextRow = current.getRow() + rowDirections[i];
+                int nextCol = current.getCol() + colDirections[i];
+
+                // check bounds and visited cells
+                if(city.inBounds(nextRow,nextCol) && !visited[nextRow][nextCol]) {
+                    Cell neighbour = city.getCell(nextRow, nextCol);
+
+                    //check connectable cells
+                    if( neighbour != null && neighbour.isConnectable()){
+
+                        visited[nextRow][nextCol] = true;
+                        queue.add(neighbour);
+                    }
+                }
+            }
+        }
     }
-
-
 }
