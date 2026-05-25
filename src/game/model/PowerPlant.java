9ba851b9211cc5ecad5043bb68a5ae0c0e64fb4c
mgulminne <melikegulminekirpat@gmail.com>
2026-05-25 21:15:16 +0300

    Added electricity distribution method to PowerPlant

--- src/game/model/PowerPlant.java
+++ src/game/model/PowerPlant.java
@@ -5,4 +5,8 @@ public class PowerPlant extends UtilityProvider{
     public PowerPlant(int row,int col){
         super(row,col,'P');
     }
+
+    public void distributeElectricity(City city,UtilityBfs bfsAlgorithm){
+        bfsAlgorithm.runBfs(city,this, "electricity");
+    }
 }
