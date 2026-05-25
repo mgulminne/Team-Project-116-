60e1b21a169815e8e4009f9e62c51c9db533a8ef
deryaalpagot <deryaalpagot@gmail.com>
2026-05-25 12:54:21 +0300

    generalize utilitybfs for all types

--- src/game/model/UtilityBfs.java
+++ src/game/model/UtilityBfs.java
@@ -1,5 +1,6 @@
 package game.model;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class UtilityBfs {
@@ -8,7 +9,12 @@ public class UtilityBfs {
     private static final int[] rowDirections = {-1,1,0,0};
     private static final int[] colDirections = {0,0,-1,1};
 
-    public void runBfs(City city, Cell startCell) {
+    public void runBfs(City city,Cell startCell, String utilityType)  {
+
+        UtilityProvider provider = (UtilityProvider) startCell;
+
+        // utility capacity
+        int remaining = provider.getCapacity();
 
         //keeps visited cells
         boolean[][] visited = new boolean[city.getRowCount()][city.getColCount()];
@@ -17,9 +23,45 @@ public class UtilityBfs {
         list.add(startCell);
         visited[startCell.getRow()][startCell.getCol()]=true;
 
-        while (!list.isEmpty()){
+        while (!list.isEmpty() && remaining > 0){
 
             Cell current = list.remove(0);
+
+            if (current.isZone()) {
+
+                Zone zone = (Zone) current;
+                int demand = 0;
+
+                if (utilityType.equals("electricity")) {
+                    demand = zone.getElectricityDemand();
+                } else if (utilityType.equals("water")) {
+                    demand = zone.getWaterDemand();
+                } else if (utilityType.equals("internet")) {
+                    demand = zone.getInternetDemand();
+                }
+
+                // amount to give
+                int given;
+
+                if (remaining < demand) {
+                    given = remaining;
+                }
+                else {
+                    given = demand;
+                }
+
+                // delivering utility
+                if (utilityType.equals("electricity")) {
+                    zone.receiveElectricity(given);
+                } else if (utilityType.equals("water")) {
+                    zone.receiveWater(given);
+                } else if (utilityType.equals("internet")) {
+                    zone.receiveInternet(given);
+                }
+
+                // update remaining capacity
+                remaining = remaining - given;
+            }
 
             //check four directions
             for (int i=0 ; i<4 ; i++){
